6b1ba74bc4d4c2f983c5063c970723e910da1b96
larioDiniz <lario.diniz@palo.com.br>
2017-10-06 10:10:52 -0300

    Aula 01

---
+++ DesignPattens2/DesignPattens2/Capitulo01/ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattens2.Capitulo01
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User id=root;Password=;Server=localhost;Database=meuBanco";
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
---
+++ DesignPattens2/DesignPattens2/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesignPattens2.Capitulo01;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattens2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "Select * trom tabela";
+        }
+    }
+}
